5202368fd2cd35624b5f2a82abcbe16f0ea55602
sebasgon89 <seba3789@gmail.com>
2023-08-17 17:57:51 -0300

    Played with projects and tasks

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.about),
     path('hello/<str:username>', views.hello),
     path('about/', views.about),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
 
 
 # Create your views here.
@@ -9,3 +10,11 @@ def hello(request, username):
 
 def about(request):
     return HttpResponse("<h2>About</h2>")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse("<h2>Task: %s </h2>" % task.title)
