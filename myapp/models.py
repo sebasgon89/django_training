a60fa7a299633fa5a4811faacb84452824dcbea8
sebasgon89 <seba3789@gmail.com>
2023-08-17 17:18:17 -0300

    Added params and data to the DB. 1:28:00

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Project(super, models.Model):
+class Project(models.Model):
     name = models.CharField(max_length=250)
 
-class Task(super, models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
