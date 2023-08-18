7af6a96f9a37c231e36f94e5141de7bb3b9382a3
sebasgon89 <seba3789@gmail.com>
2023-08-18 12:04:22 -0300

    Added user admin and admin panel stuff

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.title + " - " + self.project.name
