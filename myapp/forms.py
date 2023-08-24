2ab69bc8a6bc36e55ea6dfa5b45446cee52649df
sebasgon89 <seba3789@gmail.com>
2023-08-24 15:27:45 -0300

    Adding final styles

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label='Title')
-    description = forms.CharField(label="description", widget=forms.Textarea)
+    title = forms.CharField(label='Title', widget=forms.TextInput(attrs={'class':'input'}))
+    description = forms.CharField(label="description", widget=forms.Textarea(attrs={'class':'input'}))
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label='name')
+    name = forms.CharField(label='name', widget=forms.TextInput(attrs={'class':'input'}))
